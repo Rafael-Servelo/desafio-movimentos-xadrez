41e3db41afe2a5e6a9fc884198d54fe50f6c161a
Rafael Servelo <rafael.servelo@hotmail.com>
2025-07-25 00:12:30 -0300

    entrega nivel iniciante

--- main.c
+++ main.c
@@ -2,8 +2,31 @@
 
 int main()
 {
+    //Movimento da Torre
+    printf("\nTorre:\n");
+    for(int i= 0; i < 5; i++){
+        printf("Direita\n");
+    }
 
-    printf("Teste");
+    //Movimento do Bispo
+    printf("\nBispo:\n");
+    int i=0;
+    while (i < 5)
+    {
+        printf("Cima, Direita\n");
+        i++;
+    }
+
+    //Movimento da Rainha
+    i = 0;
+    printf("\nRainha:\n");
+    do
+    {
+        i++;
+        printf("Esquerda\n");
+    } while (i < 8);
+    
+    
 
     return 0;
 }
